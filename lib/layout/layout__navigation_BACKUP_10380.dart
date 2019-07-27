67af80320e8fb9b0bf345d22d9ec65488fd0e2d6
Lucas Ferreira <contatolucasgsf@gmail.com>
2019-07-27 12:25:36 -0300

    Login com Firebase

---
+++ lib/layout/layout__navigation_BACKUP_10380.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:aplicativo/pages/home.dart';
+import 'package:aplicativo/pages/list_view.dart';
+import 'package:aplicativo/pages/resultado.dart';
+
+import 'package:aplicativo/pages/pesquisa.dart';
+
+class LayoutNavigation extends StatefulWidget{
+
+  static LayoutNavigationState state = null;
+
+  @override
+  State<StatefulWidget> createState() {
+    state = LayoutNavigationState();
+    return state;
+  }
+
+}
+
+class LayoutNavigationState extends State<LayoutNavigation>{
+
+  int currentIndex = 0;
+  final List<Widget> pages = [
+    PesquisaPage(),
+    HomePage(),
+    ResultadoViewPage(),
+  ];
+
+  static Widget pagina_corrente = null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Center(child: Text("I²A CONNECT")),
+      ),
+
+      body: (pagina_corrente != null) ? pagina_corrente : pages[currentIndex],
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        items: [
+          BottomNavigationBarItem(icon: new Icon(Icons.home), title: new Text('Início'),),
+          BottomNavigationBarItem(icon: new Icon(Icons.search), title: new Text('Pesquisa'),),
+          BottomNavigationBarItem(icon: Icon(Icons.pets), title: Text('Touros')),
+        ],
+        onTap: (index){
+          onTabTapped(index);
+        },
+      ),
+    );
+  }
+
+  void onTabTapped(int index){
+    setState(() {
+      currentIndex = index;
+      pagina_corrente = pages[index];
+    });
+  }
+
+}
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -3,11 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:aplicativo/layout/layout.dart';
 import 'package:aplicativo/layout/layout__navigation.dart';
 import 'dart:ui';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flutter/widgets.dart' as prefix0;
 
 class LoginPage extends StatelessWidget {
   static String tag = 'login-page';
+  String _email, _senha;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -73,128 +76,141 @@ class LoginPage extends StatelessWidget {
                   color: Colors.white,
                   width: MediaQuery.of(context).size.width * 0.9,
                   height: 400,
-                  child: Column(
-                    children: <Widget>[
-                      Center(
-                        child: Row(
-                          children: <Widget>[
-                            Spacer(),
-                            Container(
-                              margin: EdgeInsets.only(top: 16, left: 0),
-                              width: 130,
-                              height: 130,
-                              decoration: new BoxDecoration(
-                                image: DecorationImage(
-                                  image: AssetImage("assets/logo.png"),
-                                  fit: BoxFit.fitHeight,
-                                ),
-                                //color: Color(0xFF244767),
-                                shape: BoxShape.circle,
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 16, top: 16),
-                              child: Container(
-                                width: 1,
-                                height: 100,
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: <Widget>[
+                        Center(
+                          child: Row(
+                            children: <Widget>[
+                              Spacer(),
+                              Container(
+                                margin: EdgeInsets.only(top: 16, left: 0),
+                                width: 130,
+                                height: 130,
                                 decoration: new BoxDecoration(
-                                  color: Colors.black38,
-                                  shape: BoxShape.rectangle
+                                  image: DecorationImage(
+                                    image: AssetImage("assets/logo.png"),
+                                    fit: BoxFit.fitHeight,
+                                  ),
+                                  //color: Color(0xFF244767),
+                                  shape: BoxShape.circle,
                                 ),
                               ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 16, top: 16),
+                                child: Container(
+                                  width: 1,
+                                  height: 100,
+                                  decoration: new BoxDecoration(
+                                    color: Colors.black38,
+                                    shape: BoxShape.rectangle
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 20, top: 16),
+                                child: Text(
+                                  "I²A CONNECT",
+                                  style: TextStyle(
+                                    fontSize: 18
+                                  ),
+                                ),
+                              ),
+                              Spacer(),
+                            ],
+                          ),
+                        ),
+                        //Login TextField
+                        Container(
+                          margin: EdgeInsets.only(top: 32, left: 16, right: 16),
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: Color(0xFFEEEEEE)
+                          ),
+                          child: TextFormField(
+                            validator: (input){
+                              if(input.isEmpty)
+                                return "Favor digitar um email!";
+                            },
+                            onSaved: (input) => _email = input,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              prefixIcon: Icon(Icons.label_important),
+                              hintText: 'Login',
+                              contentPadding: EdgeInsets.all(0),
                             ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 20, top: 16),
-                              child: Text(
-                                "I²A CONNECT",
+                          ),
+                        ),
+                        //Senha TextField
+                        Container(
+                          height: 50,
+                          margin: EdgeInsets.only(top: 8, left: 16, right: 16),
+                          decoration: BoxDecoration(
+                            color: Color(0xFFEEEEEE)
+                          ),
+                          child: TextFormField(
+                            obscureText: true,
+                            validator: (input){
+                              if(input.length < 6)
+                                return "Sua senha precisa ter ao menos 6 caracteres";
+                            },
+                            onSaved: (input) => _senha = input,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              prefixIcon: Icon(Icons.vpn_key),
+                              hintText: 'Senha',
+                              contentPadding: EdgeInsets.all(0),
+                            ),
+                          ),
+                        ),
+                        Card(
+                          margin: EdgeInsets.only(top: 24),
+                          elevation: 10,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: GestureDetector(
+                            onTap: (){
+                              login(context);
+                            },
+
+                            child: Container(
+                              height: 45,
+                              width: 180,
+
+                              decoration: BoxDecoration(
+                                  color: Color(0xFF9CEAAA),
+                                  borderRadius:
+                                  BorderRadius.all(Radius.circular(100))),
+
+                              child: Padding(
+                                padding: const EdgeInsets.only(top: 8, bottom: 8),
+                                child: Center(
+                                    child: Text("Login".toUpperCase(), style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w300,
+                                    )
+                                  ),
+                                )
+                              ),
+                            ),
+                          ),
+                        ),
+                        Spacer(),
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: Padding(
+                            padding: const EdgeInsets.only(right: 16, bottom: 8),
+                            child: Text("Esqueceu a senha?",
                                 style: TextStyle(
-                                  fontSize: 18
-                                ),
+                                  color: Color(0xFF4A86E8),
+                                )
                               ),
-                            ),
-                            Spacer(),
-                          ],
+                          )
                         ),
-                      ),
-                      //Login TextField
-                      Container(
-                        margin: EdgeInsets.only(top: 32, left: 16, right: 16),
-                        height: 50,
-                        decoration: BoxDecoration(
-                          color: Color(0xFFEEEEEE)
-                        ),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            prefixIcon: Icon(Icons.label_important),
-                            hintText: 'Login',
-                            contentPadding: EdgeInsets.all(0),
-                          ),
-                        ),
-                      ),
-                      //Senha TextField
-                      Container(
-                        height: 50,
-                        margin: EdgeInsets.only(top: 8, left: 16, right: 16),
-                        decoration: BoxDecoration(
-                          color: Color(0xFFEEEEEE)
-                        ),
-                        child: TextField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            prefixIcon: Icon(Icons.vpn_key),
-                            hintText: 'Senha',
-                            contentPadding: EdgeInsets.all(0),
-                          ),
-                        ),
-                      ),
-                      Card(
-                        margin: EdgeInsets.only(top: 24),
-                        elevation: 10,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(100),
-                        ),
-                        child: GestureDetector(
-                          onTap: (){
-                            login(context);
-                          },
-
-                          child: Container(
-                            height: 45,
-                            width: 180,
-
-                            decoration: BoxDecoration(
-                                color: Color(0xFF9CEAAA),
-                                borderRadius:
-                                BorderRadius.all(Radius.circular(100))),
-
-                            child: Padding(
-                              padding: const EdgeInsets.only(top: 8, bottom: 8),
-                              child: Center(
-                                  child: Text("Login".toUpperCase(), style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w300,
-                                  )
-                                ),
-                              )
-                            ),
-                          ),
-                        ),
-                      ),
-                      Spacer(),
-                      Align(
-                        alignment: Alignment.bottomRight,
-                        child: Padding(
-                          padding: const EdgeInsets.only(right: 16, bottom: 8),
-                          child: Text("Esqueceu a senha?",
-                              style: TextStyle(
-                                color: Color(0xFF4A86E8),
-                              )
-                            ),
-                        )
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
@@ -237,10 +253,18 @@ class LoginPage extends StatelessWidget {
     );
   }
 
-  void login(context) {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => LayoutNavigation()));
-    LayoutNavigationState.pagina_corrente = null;
-
+  Future<void> login(context) async {
+    final formState = _formKey.currentState;
+    if(formState.validate()){
+      formState.save();
+      try{
+        FirebaseUser user = await FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _senha);
+        Navigator.push(context, MaterialPageRoute(builder: (context) => LayoutNavigation()));
+        LayoutNavigationState.pagina_corrente = null;
+      }
+      catch(e){
+        print(e.message);
+      }
+    }
   }
-
 }
